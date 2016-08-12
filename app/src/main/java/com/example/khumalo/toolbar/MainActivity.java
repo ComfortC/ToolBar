513ee183a783084aaedf5fe17eb845dbb0ef8546
ComfortC <comfortchino@gmail.com>
2016-08-12 20:10:24 +0200

    Initial Commit Toolbar

---
+++ app/src/main/java/com/example/khumalo/toolbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.khumalo.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar topToolBar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
+        topToolBar.setLogo(R.drawable.ic_toobar_icon);
+        setTitle(null);
+    }
+}
