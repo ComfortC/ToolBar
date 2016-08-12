ea3e4ab81cdeae074d2e8326871379a149cd29e6
ComfortC <comfortchino@gmail.com>
2016-08-12 20:28:55 +0200

    Allowed The Menu To Appear

--- app/src/main/java/com/example/khumalo/toolbar/MainActivity.java
+++ app/src/main/java/com/example/khumalo/toolbar/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.khumalo.toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(topToolBar);
         topToolBar.setLogo(R.drawable.ic_toobar_icon);
         setTitle(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       int id =  item.getItemId();
+        if(id == R.id.action_edit_list_name){
+            Toast.makeText(this,"Sending Message",Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 }
